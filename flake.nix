92c7e2923413a66cf5ca4f74b4f58273b2b74f20
Apile Tyumre <apiletyumre@iridescentsun.com>
2024-11-13 00:14:54 +0200

    Add Nix flake
    Add nix flake with devshell and package build for the project

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Stream Watcher flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "Stream Watcher";
+          version = "0.1.0";
+
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          buildInputs = with pkgs; [
+            openssl
+          ];
+
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+
+        };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+          packages = with pkgs; [
+            rust-bin.stable.latest.default
+            twitch-cli
+            mpv
+            streamlink
+          ];
+        };
+      }
+    );
+}
